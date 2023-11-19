4f83f25ac79356490e9cfa024aa6bc7c40241447
Ferreiraview <81189276+Ferreiraview@users.noreply.github.com>
2023-11-19 22:20:01 +0000

    main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import Typebot from 'https://cdn.jsdelivr.net/npm/@typebot.io/js@0.2.22/dist/web.js'; 
+import Typebot from 'https://cdn.jsdelivr.net/npm/@typebot.io/js@0.2.22/dist/web.js';
 
 class Loader {
-    static loadedFiles: string[] = [];
+    static loadedFiles = [];
 
-    static load(type: 'video' | 'img', src: string, onload: () => void): void {
+    static load(type, src, onload) {
         if (this.loadedFiles.includes(src)) {
             onload();
             return;
@@ -13,11 +13,10 @@ class Loader {
         element.src = src;
 
         if (type === 'video') {
-            const videoElement = element as HTMLVideoElement;
-            videoElement.muted = true;
-            videoElement.autoplay = true;
-            videoElement.playsinline = true;
-            videoElement.addEventListener('canplaythrough', () => this.onMediaLoaded(src, onload, videoElement));
+            element.muted = true;
+            element.autoplay = true;
+            element.playsinline = true;
+            element.addEventListener('canplaythrough', () => this.onMediaLoaded(src, onload, element));
         } else if (type === 'img') {
             element.addEventListener('load', () => this.onMediaLoaded(src, onload, element));
         } else {
@@ -27,18 +26,18 @@ class Loader {
         document.body.appendChild(element); // Temporariamente para carregar
     }
 
-    static onMediaLoaded(src: string, onload: () => void, element: HTMLElement): void {
+    static onMediaLoaded(src, onload, element) {
         this.loadedFiles.push(src);
         onload();
 
         if (element.tagName === 'VIDEO') {
-            (element as HTMLVideoElement).play().catch(e => console.error("Erro ao tentar autoplay: ", e));
+            element.play().catch(e => console.error("Erro ao tentar autoplay: ", e));
         }
 
         document.body.removeChild(element); // Remove após carregar
     }
 
-    static createElementByType(type: 'video' | 'img'): HTMLVideoElement | HTMLImageElement {
+    static createElementByType(type) {
         if (type === 'img') {
             return document.createElement('img');
         } else if (type === 'video') {
@@ -50,24 +49,83 @@ class Loader {
 }
 
 const bg = document.getElementById('bg');
-if (bg) {
-    function changeBg(type: 'video' | 'img', src: string): void {
-        const newElement = Loader.createElementByType(type);
-        if (type === 'video') {
-            const videoElement = newElement as HTMLVideoElement;
-            videoElement.loop = true;
-            videoElement.autoplay = true;
-            videoElement.muted = true;
-            videoElement.playsinline = true;
-            videoElement.play();
-        }
-        newElement.src = src;
-        bg.appendChild(newElement);
-        newElement.classList.add('bg-content');
-        while (bg.children.length > 1) {
-            bg.removeChild(bg.children[0]);
-        }
+function changeBg(type, src) {
+    const newElement = Loader.createElementByType(type);
+    if (type === 'video') {
+        newElement.loop = true;
+        newElement.autoplay = true;
+        newElement.muted = true;
+        newElement.playsinline = true;
+        newElement.play();
+    }
+    newElement.src = src;
+    bg.appendChild(newElement);
+    newElement.classList.add('bg-content');
+    while (bg.children.length > 1) {
+        bg.removeChild(bg.children[0]);
     }
 }
 
-// Restante do código para Typebot e geração de bots...
+let typeBotIndex = 0;
+function runTypeBot(asset, typebotInfo, next) {
+    Loader.load(asset.type, asset.src, () => {
+        changeBg(asset.type, asset.src);
+        if (typeBotIndex > 0) {
+            document.getElementById('typebots').removeChild(document.getElementById(`typebot-${typeBotIndex - 1}`));
+        }
+        const typebotDiv = document.createElement('typebot-standard');
+        typebotDiv.id = `typebot-${typeBotIndex}`;
+        typebotDiv.classList.add('typebot');
+        document.getElementById('typebots').appendChild(typebotDiv);
+        typeBotIndex += 1;
+        Typebot.initStandard({
+            id: typebotDiv.id,
+            typebot: typebotInfo.bot,
+            apiHost: typebotInfo.host,
+            onEnd: () => {
+                if (next.length > 0) {
+                    setTimeout(() => {
+                        typebotDiv.style.display = 'none';
+                        runTypeBot(...next);
+                    }, 500);
+                }
+            }
+        });
+    })
+}
+
+function generateBotsInFlow(bots) {
+    const flow = [];
+    let nowArr = flow;
+    for (const bot of bots) {
+        nowArr.push(bot.asset, bot.bot);
+        const newNowArr = [];
+        nowArr.push(newNowArr);
+        nowArr = newNowArr;
+    }
+    return flow;
+}
+
+(() => {
+    Loader.load('img', 'public/0.jpg', () => { });
+    Loader.load('video', 'public/rain.mp4', () => { });
+
+    const bot1 = {
+        asset: { type: 'img', src: 'public/0.jpg' },
+        bot: {
+            bot: 'welcome-eer92l9',
+            host: 'https://app.ferreiraview.com'
+        }
+    };
+    const bot2 = {
+        asset: { type: 'video', src: 'public/rain.mp4' },
+        bot: {
+            bot: 'id2-woken44',
+            host: 'https://app.ferreiraview.com'
+        }
+    };
+
+    const bots = [bot1, bot2];
+
+    runTypeBot(...generateBotsInFlow(bots));
+})();
